30d58c5267967a80d2b7c47779908ea87962c0a2
Kat <kyevsukov@gmail.com>
2022-02-16 17:32:04 -0500

    added hero banner to app

--- cocktails-ui/src/App.js
+++ cocktails-ui/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Alert } from 'react-bootstrap'
+import Jumbotron from 'bootstrap';
+import heroImg from './images/hero-img.png'
 
 function App() {
   return (
     <div className="App">
+      <div class="jumbotron">
+      <img src={heroImg} class="img-fluid" alt="bartender standing at a bar stirring a cocktail" />
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
       <header className="App-header">
-        Hello
+        <Alert variant="secondary">This is a button</Alert>
+        <Button>hello</Button>
       </header>
     </div>
   );
