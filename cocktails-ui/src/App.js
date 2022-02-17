277f968ef5ed601f264368fcfde196ceb2d971d8
Kat <kyevsukov@gmail.com>
2022-02-17 12:30:33 -0500

    feat: added hero image to site

--- cocktails-ui/src/App.js
+++ cocktails-ui/src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import {Button, Alert } from 'react-bootstrap'
-import Jumbotron from 'bootstrap';
-import heroImg from './images/hero-img.png'
+import "./App.css";
+import { Button, Alert } from "react-bootstrap";
+import Jumbotron from "bootstrap";
+import heroImg from "./images/hero-img.png";
 
 function App() {
   return (
     <div className="App">
-      <div class="jumbotron">
-      <img src={heroImg} class="img-fluid" alt="bartender standing at a bar stirring a cocktail" />
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-</div>
+        <img
+          src={heroImg}
+          class="img-fluid"
+          alt="bartender standing at a bar stirring a cocktail"
+        />
       <header className="App-header">
         <Alert variant="secondary">This is a button</Alert>
         <Button>hello</Button>
