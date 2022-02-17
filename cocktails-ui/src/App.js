f9cda8c30a4042a6167fa11872a73914785a95ee
Kat <kyevsukov@gmail.com>
2022-02-17 12:49:38 -0500

    added login form

--- cocktails-ui/src/App.js
+++ cocktails-ui/src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import { Button, Alert } from "react-bootstrap";
-import Jumbotron from "bootstrap";
+import Login from './components/Login'
 import heroImg from "./images/hero-img.png";
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
         <img
           src={heroImg}
           class="img-fluid"
           alt="bartender standing at a bar stirring a cocktail"
         />
-      <header className="App-header">
-        <Alert variant="secondary">This is a button</Alert>
-        <Button>hello</Button>
       </header>
+        <Login />
+        {/* <Alert variant="secondary">This is a button</Alert>
+        <Button>hello</Button> */}
+      
     </div>
   );
 }
