df569590058579093e1dacfe8213aee94e09db12
Kat <kyevsukov@gmail.com>
2022-03-07 12:20:48 -0500

    set up global vars and axios with auth

---
+++ cocktails-ui/src/config/index.js
@@ -0,0 +1,5 @@
+const globalVars = {
+    API_URL: process.env.API_URL
+}
+
+export default globalVars
---
+++ cocktails-ui/src/utils/axiosWithAuth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { API_URL } from '../config'
+
+const axiosWithAuth = () => {
+    const token = localStorage.getItem("token");
+
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        baseURL: API_URL
+    })
+}
+
+export default axiosWithAuth
