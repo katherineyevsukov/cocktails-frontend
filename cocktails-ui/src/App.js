502aa5cd52513a4135eff789d2f44ea8d8e712e9
Kat <kyevsukov@gmail.com>
2022-03-07 16:16:14 -0500

    updated login form styling

--- cocktails-ui/src/App.js
+++ cocktails-ui/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Button, Alert } from "react-bootstrap";
-import Landing from './components/Landing'
+import Landing from "./components/Landing";
 import heroImg from "./images/hero-img.jpg";
-import { API_URL } from '../src/config'
+import { API_URL } from "../src/config";
 import axios from "axios";
 
 function App() {
@@ -15,10 +15,9 @@ function App() {
           alt="bartender wearing a white shirt and black apron standing at a well-stocked bar stirring a cocktail"
         />
       </header>
-        <Landing />
-        {/* <Alert variant="secondary">This is a button</Alert>
+      <Landing />
+      {/* <Alert variant="secondary">This is a button</Alert>
         <Button>hello</Button> */}
-      
     </div>
   );
 }
