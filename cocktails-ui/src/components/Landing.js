985e41329e05771beac5e86db9fc421f977c80a2
Kat <kyevsukov@gmail.com>
2022-03-12 15:02:38 -0500

    added jumbotron

--- cocktails-ui/src/components/Landing.js
+++ cocktails-ui/src/components/Landing.js
@@ -1,17 +1,41 @@
 import "./../styles/landing.css";
-import React, { useState } from 'react'
-import Login from './Login'
-import Signup from './Signup'
+import React, { useState } from "react";
+import Login from "./Login";
+import Signup from "./Signup";
 
-function Landing(){
-    const [signup, setSignup] = useState(false)
+function Landing() {
+  const [signup, setSignup] = useState(false);
 
-    return (
-        <div>
-            {!signup ? <Login setSignup={setSignup}/> : null}
-            {signup ? <Signup setSignup={setSignup}/> : null}
-        </div>
-    )
+  return (
+    <>
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr class="my-4"></hr>
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">
+            Learn more
+          </a>
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">
+            Learn more
+          </a>
+        </p>
+      </div>
+      <div>
+        {!signup ? <Login setSignup={setSignup} /> : null}
+        {signup ? <Signup setSignup={setSignup} /> : null}
+      </div>
+    </>
+  );
 }
 
-export default Landing
+export default Landing;
