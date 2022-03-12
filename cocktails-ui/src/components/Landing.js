6b8dcc57aab4beef6d9480af11f95e06e398eaa8
Kat <kyevsukov@gmail.com>
2022-03-12 15:23:40 -0500

    linked jumbotron buttons to login and signup

--- cocktails-ui/src/components/Landing.js
+++ cocktails-ui/src/components/Landing.js
@@ -2,6 +2,7 @@ import "./../styles/landing.css";
 import React, { useState } from "react";
 import Login from "./Login";
 import Signup from "./Signup";
+import { Link } from "react-router-dom";
 
 function Landing() {
   const [signup, setSignup] = useState(false);
@@ -9,7 +10,7 @@ function Landing() {
   return (
     <>
       <div class="jumbotron">
-        <h1 class="display-4">Hello, world!</h1>
+        <h1 class="display-4">Welcome, cocktail creators!</h1>
         <p class="lead">
           This is a simple hero unit, a simple jumbotron-style component for
           calling extra attention to featured content or information.
@@ -20,14 +21,26 @@ function Landing() {
           out within the larger container.
         </p>
         <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">
-            Learn more
-          </a>
+          <Link
+            to="/register"
+            class="btn btn-primary btn-lg landing-button"
+            role="button"
+          >
+            Sign Up
+          </Link>
+        </p>
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
         </p>
         <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">
-            Learn more
-          </a>
+          <Link
+            to="/login"
+            class="btn btn-primary btn-lg landing-button"
+            role="button"
+          >
+            Login
+          </Link>
         </p>
       </div>
       <div>
