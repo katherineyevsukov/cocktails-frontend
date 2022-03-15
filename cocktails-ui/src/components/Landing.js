e3939d5eb8170c9d90de0b6d230a84e1e7532761
Kat <kyevsukov@gmail.com>
2022-03-15 14:33:18 -0400

    updated jumbotron description

--- cocktails-ui/src/components/Landing.js
+++ cocktails-ui/src/components/Landing.js
@@ -8,8 +8,21 @@ function Landing() {
       <div class="jumbotron">
         <h1 class="display-4">Welcome, cocktail creators!</h1>
         <p class="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
+          Say goodbye to your bartender's notebook! Handshaken allow bartenders
+          to easily keep track of cocktails they create, without the hassle of
+          carrying around a notebook &#40;or the fear of destroying it with a
+          spill &#41;. With Handshaken bartenders can upload photos, detail
+          ingredients &amp; steps, and update recipes as necessary.
+        </p>
+        <p class="lead">
+          Looking for inspiration? Look no further! Handshaken provides an
+          easy-to-use search feature, where bartenders can find cocktails
+          created by other users.
+        </p>
+        <p class="lead">
+          Are you a bar manager? Organize your menus with us! With Handshaken,
+          a manager can create bars, invite their staff to join it and easily
+          share and update bar-exclusive recipes and menus.
         </p>
         <hr class="my-4"></hr>
         {/* <p>
@@ -24,9 +37,7 @@ function Landing() {
             Sign Up
           </Link>
         </p>
-        <p>
-          Already have an account? Click below to login:
-        </p>
+        <p>Already have an account? Click below to login:</p>
         <p class="lead">
           <Link
             to="/login"
