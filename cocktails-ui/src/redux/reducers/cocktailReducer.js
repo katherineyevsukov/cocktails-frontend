32de640b4fb302667ca0a65213dd1b95ad82efdd
Kat <kyevsukov@gmail.com>
2022-03-21 15:01:07 -0400

    updated cocktail reducer cases

--- cocktails-ui/src/redux/reducers/cocktailReducer.js
+++ cocktails-ui/src/redux/reducers/cocktailReducer.js
@@ -3,76 +3,34 @@ import { FETCH_USER_COCKTAILS_START, FETCH_USER_COCKTAILS_SUCCESS, FETCH_USER_CO
 const initialState = {
     cocktails: [],
     cocktailsLoading: false,
+    coctailErrorMessage: '',
     cocktailIngredients: [],
     cocktailSteps: [],
     cocktailPhoto: ''
 }
 
 
-// export default function authReducer(state = initialState, action) {
-//     const { type, payload } = action;
-//     switch (type) {
-//       case LOGIN_START:
-//         return {
-//           ...state,
-//           isLoading: true,
-//           isLoggedin: false,
-//           user: null,
-//           authSuccessMessage: null,
-//           authErrorMessage: null,
-//         };
-//       case LOGIN_SUCCESS:
-//         return {
-//           ...state,
-//           isLoggedin: true,
-//           user: payload.token,
-//           isLoading: false,
-//           authSuccessMessage: payload.message,
-//           authErrorMessage: null,
-//         };
-//       case LOGIN_FAIL:
-//         return {
-//           ...state,
-//           isLoggedin: false,
-//           user: null,
-//           isLoading: false,
-//           authErrorMessage: payload.message,
-//           authSuccessMessage: null,
-//         };
-//       case SIGNUP_START:
-//         return {
-//           ...state,
-//           isLoading: true,
-//           isLoggedin: false,
-//           user: null,
-//           authErrorMessage: null,
-//           authSuccessMessage: null,
-//         };
-//       case SIGNUP_SUCCESS:
-//         return {
-//           ...state,
-//           isLoggedin: false,
-//           user: null,
-//           isLoading: false,
-//           authSuccessMessage: payload.message,
-//           authErrorMessage: null,
-//         };
-//       case SIGNUP_FAIL:
-//         return {
-//           ...state,
-//           isLoggedin: false,
-//           user: null,
-//           isLoading: false,
-//           authErrorMessage: payload.message,
-//           authSuccessMessage: null,
-//         };
-//       case MESSAGE_RESET:
-//         return {
-//           ...state,
-//           authErrorMessage: null,
-//           authSuccessMessage: null,
-//         };
-//       default:
-//         return state;
-//     }
-//   }
+export default function cocktailReducer(state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case FETCH_USER_COCKTAILS_START:
+        return {
+          ...state,
+          cocktailsLoading: true,
+        };
+      case FETCH_USER_COCKTAILS_SUCCESS:
+        return {
+          ...state,
+          cocktails: payload.cocktails,
+          cocktailsLoading: false,
+        };
+      case FETCH_USER_COCKTAILS_FAIL:
+        return {
+          ...state,
+          cocktailsLoading: false,
+          cocktailErrorMessage: payload.message
+        };
+      default:
+        return state;
+    }
+  }
