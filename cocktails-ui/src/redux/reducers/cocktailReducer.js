f82c0d8bf08647a43ef41b95e4c94f6256323dc4
Kat <kyevsukov@gmail.com>
2022-03-22 18:28:30 -0400

    set cocktail reducer case created

--- cocktails-ui/src/redux/reducers/cocktailReducer.js
+++ cocktails-ui/src/redux/reducers/cocktailReducer.js
@@ -1,37 +1,46 @@
-import { FETCH_USER_COCKTAILS_START, FETCH_USER_COCKTAILS_SUCCESS, FETCH_USER_COCKTAILS_FAIL} from './../actions/cocktailActions'
+import {
+  FETCH_USER_COCKTAILS_START,
+  FETCH_USER_COCKTAILS_SUCCESS,
+  FETCH_USER_COCKTAILS_FAIL,
+  SET_COCKTAIL,
+} from "./../actions/cocktailActions";
 
 const initialState = {
-    cocktails: [],
-    cocktailsLoading: false,
-    cocktailErrorMessage: '',
-    cocktail: {},
-    cocktailIngredients: [],
-    cocktailSteps: [],
-}
-
+  cocktails: [],
+  cocktailsLoading: false,
+  cocktailErrorMessage: "",
+  cocktail: {},
+  cocktailIngredients: [],
+  cocktailSteps: [],
+};
 
 export default function cocktailReducer(state = initialState, action) {
-    const { type, payload } = action;
-    switch (type) {
-      case FETCH_USER_COCKTAILS_START:
-        return {
-          ...state,
-          cocktailsLoading: true,
-          cocktails: [],
-        };
-      case FETCH_USER_COCKTAILS_SUCCESS:
-        return {
-          ...state,
-          cocktails: payload.cocktails,
-          cocktailsLoading: false,
-        };
-      case FETCH_USER_COCKTAILS_FAIL:
-        return {
-          ...state,
-          cocktailsLoading: false,
-          cocktailErrorMessage: payload.message
-        };
-      default:
-        return state;
-    }
+  const { type, payload } = action;
+  switch (type) {
+    case FETCH_USER_COCKTAILS_START:
+      return {
+        ...state,
+        cocktailsLoading: true,
+        cocktails: [],
+      };
+    case FETCH_USER_COCKTAILS_SUCCESS:
+      return {
+        ...state,
+        cocktails: payload.cocktails,
+        cocktailsLoading: false,
+      };
+    case FETCH_USER_COCKTAILS_FAIL:
+      return {
+        ...state,
+        cocktailsLoading: false,
+        cocktailErrorMessage: payload.message,
+      };
+      case SET_COCKTAIL:
+      return {
+        ...state,
+        cocktail: payload.cocktail
+      };
+    default:
+      return state;
   }
+}
