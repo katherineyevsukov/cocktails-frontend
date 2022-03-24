bcf480e713d05882870ab56417695a8c275e9ce8
Kat <kyevsukov@gmail.com>
2022-03-24 12:54:33 -0400

    added and configured redux persist

--- cocktails-ui/src/index.js
+++ cocktails-ui/src/index.js
@@ -7,20 +7,35 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 import reducer from "./redux/reducers";
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+// const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
